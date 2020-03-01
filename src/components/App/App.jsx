daf70e91be2ff79510cb9c760ceac867cc5afaaa
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-03-01 10:16:13 -0800

    added search bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Axios from 'axios';
 
-const port = 8081;
+import { makeStyles, Typography, Paper, Divider, Grid } from '@material-ui/core';
 
-const url = `http://localhost:${port}`;
+import SearchBar from '../searchBar';
 
-const params = new URLSearchParams(window.location.search);
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3)
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing(1)
+  },
+  divider: {
+    margin: theme.spacing(2, 0)
+  },
+  Bolder: {
+    fontWeight: 'bolder'
   }
+}));
 
-  componentDidMount() {}
+export default function CSSGrid() {
+  const classes = useStyles();
+  const [keyWord, setKeyWord] = useState('');
+  const [returnDataNumber, setReturnDataNumber] = useState(16);
 
-  render() {
-    const { dataObject } = this.state;
-    return (
-      <>
-        <div>hello</div>
-      </>
-    );
-  }
+  const getAnimatData = () => {
+    Axios.get(`https://api.jikan.moe/v3/search/anime?q=${keyWord}&limit=${returnDataNumber}`)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {}, [keyWord]);
+
+  const handleButtonSend = () => {
+    if (keyWord === '') {
+      window.alert('entry something');
+    } else {
+      getAnimatData();
+    }
+  };
+
+  const handleKeyDown = inputValue => {
+    setKeyWord(inputValue);
+    console.log(typeof inputValue);
+    if (window.event.keyCode === 13 && keyWord === '') {
+      window.alert('entry something');
+    } else if (window.event.keyCode === 13 && keyWord !== '') {
+      getAnimatData();
+    }
+  };
+
+  return (
+    <div>
+      <Typography className={classes.Bolder} variant="h1" align="center" color="primary">
+        My Anime List
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <SearchBar handleSearch={handleKeyDown} handleButtonSend={handleButtonSend} />
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=4</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=4</Paper>
+        </Grid>
+      </Grid>
+      <Divider className={classes.divider} />
+    </div>
+  );
 }
-
-export default App;
---
+++ src/components/searchBar.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1
+  },
+  title: {
+    display: 'none',
+    width: '23%',
+    [theme.breakpoints.up('xs')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    display: 'flex',
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: '80%'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '100%'
+    }
+  }
+}));
+
+export default function SearchBar(props) {
+  const classes = useStyles();
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Anime Name:
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Entry your text here"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+              onKeyDown={() => props.handleSearch(inputValue)}
+              value={inputValue}
+              onChange={e => setInputValue(e.target.value)}
+            />
+          </div>
+          <IconButton aria-label="delete" onClick={() => props.handleButtonSend()}>
+            <SendIcon color="secondary" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
