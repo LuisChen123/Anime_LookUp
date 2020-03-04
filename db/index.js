67178d6698d42451bc09a0e76f39cc75b4f318ff
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-03-04 08:02:48 -0800

    chage

--- db/index.js
+++ db/index.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const MongoDBPassWord = require('../config');
-
-const DB_URL = `mongodb+srv://luis:${MongoDBPassWord}@cluster0-vv08g.mongodb.net/test?retryWrites=true&w=majority`;
-=======
 const DB_URL =
   'mongodb+srv://luis:c82806817@cluster0-vv08g.mongodb.net/test?retryWrites=true&w=majority/anime';
->>>>>>> 6e82122ef236f13ca04faa0975479b8fffb21ec6
 /** * mongoDb Url */
 mongoose.connect(DB_URL);
 
