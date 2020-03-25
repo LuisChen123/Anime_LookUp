dd661660ef3c969712379361a364aae676daf4ec
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-03-25 05:29:13 -0700

    fiexed bug in the detial page

--- src/components/childComponents/AnimeDetailPage.jsx
+++ src/components/childComponents/AnimeDetailPage.jsx
@@ -3,7 +3,6 @@ import Axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   root: {
